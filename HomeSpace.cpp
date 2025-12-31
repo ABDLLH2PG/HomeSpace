52a43a81a435fe5bc71f17fabf7e7b87a428c762
ABDLLH2PG <ABDLLH2PG@gmail.com>
2025-12-31 14:40:37 +0300

    Lesson #14 - Declaration Vs Definition

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
-//Lesson #13, BitWise OR operator
+// function declaration
+int Sum2Numbers(int, int);
+
 
 int main()
 {
-	cout << "Result: " << (12 | 25);
-	cout << endl;
+	cout << "Result: " << Sum2Numbers(10, 20) << endl;
 
 
 
 	return 0;
 }
+
+// function definition
+int Sum2Numbers(int Num1, int Num2)
+{
+	return Num1 + Num2;
+}
+
