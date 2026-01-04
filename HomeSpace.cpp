c07a8b578f2846162ee017cda1b60d03571e67f1
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-04 14:45:39 +0300

    Lesson #20 - Static Variables

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 
+//Lesson #20 - Static Variables
+void MyFunc()
+{
+	static int Number = 1;
+	cout << "Value of Number: " << Number << "\n";
+
+	Number++;
+}
+
 int main()
 {
-
+	MyFunc();
+	MyFunc();
+	MyFunc();
 
 
 	return 0;
