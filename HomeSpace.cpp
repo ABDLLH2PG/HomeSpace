86433c9787b1440d213626077d321bc704bd347a
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-04 22:13:22 +0300

    Lesson #24 - Float Format (printf)

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 using namespace std;
 
-//Lesson #23 - Integer Format(printf) [ œ—Ì» «÷«›Ì „‰ ÃÌ„Ì 2]
+//Lesson #24 - Float Format (printf)
 int main()
 {
-	int Price = 50, Quantity = 3;
+	float PI = 3.14159265;
+	
+	//Precision specifiication
+	printf("Precision specification of %.*f \n", 1, PI);
+	printf("Precision specification of %.*f \n", 2, PI);
+	printf("Precision specification of %.*f \n", 3, PI);
+	printf("Precision specification of %.*f \n", 4, PI);
 
-	printf("Price: %d, Qty: %d, Total: %0*d \n", Price, Quantity, 4, Price * Quantity);
+	float x = 7.0, y = 9.0;
+	printf("\nThe float division is : %.3f / %.3f = %.3f \n\n", x, y, x / y);
 
+	double d = 12.45;
+	printf("The double value is : %.3f \n", d);
+	printf("The double value is : %.4f \n", d);
 
+	
 	return 0;
 }
