021424f0c5cb6b1fbaaa338e6f19697de5f1c0d3
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-05 00:30:37 +0300

    Lesson #25 - String and Char Format(printf) [تدريب من جيمي 1]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
 using namespace std;
 
-//Lesson #25 - String and Char Format(printf)
+//Lesson #25 - String and Char Format(printf) [ œ—Ì» „‰ ÃÌ„Ì 1]
 int main()
 {
-	char Name[] = "Mohammed Abu-Hadhoud";
-	char SchoolName[] = "Programming Advices";
+	char EmployeeName[] = "Ahmed";
+	char Grade = 'A';
 
-	// print string and String
-	printf("Dear %s, How are you?\n\n", Name);
-	printf("Welcome to %s School!\n\n", SchoolName);
+	printf("Employee: %s, Performance: %c \n", EmployeeName, Grade);
 
-	char c = 'S';
-
-	printf("Setting the width of c :%*c \n", 1, c);
-	printf("Setting the width of c :%*c \n", 2, c);
-	printf("Setting the width of c :%*c \n", 3, c);
-	printf("Setting the width of c :%*c \n", 4, c);
-	printf("Setting the width of c :%*c \n", 5, c);
 
 
 	return 0;
