221dda0541adf5fbb603e0372952293f7a702414
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-08 21:40:05 +0300

    Lesson #28 - Introduction, Declaration and Initialization

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,40 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-//Lesson #27 - Two Dimensional Arrays [HomeWork Solution]
-void StoreMultiplicationTable(int Table[10][10])
-{
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-		{
-			Table[i][j] = ((i + 1) * (j + 1));
-		}
-	}
-}
-
-void PrintMultiplicationTable(int Table[10][10])
-{
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-		{
-			printf("%0*d ", 2, Table[i][j]);
-		}
-
-		cout << endl;
-	}
-}
-
+//Lesson #28 - Introduction, Declaration and Initialization
 int main()
 {
-	// int Table[Rows][Cols];
-	int Table[10][10];
+	// std::vector<T> vector_name;
 
-	StoreMultiplicationTable(Table);
+	vector <int> vNumbers = { 10, 20, 30, 40, 50 };
 
-	PrintMultiplicationTable(Table);
+	cout << "Numbers Vector = ";
 
+	for (int &Number : vNumbers)
+	{
+		cout << Number << "  ";
+	}
+
+	cout << endl;
 
 	return 0;
 }
