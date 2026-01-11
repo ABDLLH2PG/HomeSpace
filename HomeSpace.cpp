1c21c10d0e9c627a154a03529a971c94aa999ef5
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-11 21:45:18 +0300

    Lesson #29 - Add elements [HomeWork]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-// Lesson #29 - Add elements
-int main()
+// Lesson #29 - Add elements [HomeWork]
+void ReadNumbers(vector <int> & vNumbers)
 {
-	vector <int> vNumbers;
+	char ReadMore = 'Y';
+	int Number = 0;
 
-	vNumbers.push_back(10);
-	vNumbers.push_back(20);
-	vNumbers.push_back(30);
-	vNumbers.push_back(40);
-	vNumbers.push_back(50);
+	while (ReadMore == 'Y' || ReadMore == 'y')
+	{
+		cout << "Please enter a number? ";
+		cin >> Number;
 
+		vNumbers.push_back(Number);
+
+		cout << "\nDo you want to read more numbers? Y/N ? ";
+		cin >> ReadMore;
+
+	}
+}
+
+void PrintVectorNumbers(vector <int> & vNumbers)
+{
 	cout << "Numbers Vector: \n\n";
 
-	//ranged loop
-	for (int &Number : vNumbers)
+	for (int Number : vNumbers)
 	{
 		cout << Number << endl;
 	}
 
 	cout << endl;
-	 
+}
+
+
+int main()
+{
+	vector <int> vNumbers;
+
+	ReadNumbers(vNumbers);
+	PrintVectorNumbers(vNumbers);
+
+
 	return 0;
 }
