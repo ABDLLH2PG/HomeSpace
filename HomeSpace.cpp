c5e5d72caf7b3a268c01bf78e8758436eee56cc7
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-11 21:48:32 +0300

    Lesson #29 - Add elements [Al HomeWork]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,34 +1,64 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-// Lesson #29 - Add elements [HomeWork]
+//Lesson #29 - Add elements [Al HomeWork]
 void ReadNumbers(vector <int> & vNumbers)
 {
 	char ReadMore = 'Y';
 	int Number = 0;
 
-	while (ReadMore == 'Y' || ReadMore == 'y')
+	do
 	{
-		cout << "Please enter a number? ";
+		cout << "Please enter a number ? ";
 		cin >> Number;
 
 		vNumbers.push_back(Number);
 
-		cout << "\nDo you want to read more numbers? Y/N ? ";
+		cout << "\nDo you want to add more numbers ? Y/N ? ";
 		cin >> ReadMore;
 
+	} while (ReadMore == 'Y' || ReadMore == 'y');
+
+}
+
+int SumVectorNumbers(vector <int> & vNumbers)
+{
+	int Sum = 0;
+
+	for (int Number : vNumbers)
+	{
+		Sum += Number;
 	}
+
+	return Sum;
+}
+
+int MaxNumber(vector <int> & vNumbers)
+{
+	int Max = vNumbers[0];
+
+	for (int Number : vNumbers)
+	{
+		if (Number > Max)
+		{
+			Max = Number;
+		}
+	}
+
+	return Max;
 }
 
 void PrintVectorNumbers(vector <int> & vNumbers)
 {
-	cout << "Numbers Vector: \n\n";
-
-	for (int Number : vNumbers)
+	for (int i = 0; i < vNumbers.size(); i++)
 	{
-		cout << Number << endl;
+		cout << vNumbers[i];
+
+		if (i < vNumbers.size() - 1)
+		{
+			cout << ", ";
+		}
 	}
 
 	cout << endl;
@@ -40,7 +70,13 @@ int main()
 	vector <int> vNumbers;
 
 	ReadNumbers(vNumbers);
+
+	cout << "\nNumbers Vector: ";
 	PrintVectorNumbers(vNumbers);
+
+	cout << "Total Sum: " << SumVectorNumbers(vNumbers) << endl;
+
+	cout << "Maximum Number: " << MaxNumber(vNumbers) << endl;
 
 
 	return 0;
