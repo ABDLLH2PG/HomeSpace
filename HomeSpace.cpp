16584d551aaac3a74936d54754c918353e525fd4
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-13 15:02:04 +0300

    Lesson #33 - Call ByRef/ByVal Important Review.

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,30 +1,23 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-// Lesson #32 - Vector Functions
+// Lesson #33 - Call ByRef/ByVal Important Review. 
+void Function1(int &x)
+{
+	x++;
+}
+
+
 int main()
 {
-	vector <int> vNumbers;
+	int a = 10;
 
-	vNumbers.push_back(10);
-	vNumbers.push_back(20);
-	vNumbers.push_back(30);
-	vNumbers.push_back(40);
-	vNumbers.push_back(50);
+	cout << a << endl;
+	cout << &a << endl;
 
+	Function1(a);
 
-	cout << "First Element: " << vNumbers.front() << endl;
-	cout << "Last Element: " << vNumbers.back() << endl;
-
-	//returns the number of elements present in the vector
-	cout << "Size: " << vNumbers.size() << endl;
-
-	//check the overall size of a vector
-	cout << "Capacity : " << vNumbers.capacity() << endl;
-
-	//return 1 (true) if the vector is empty
-	cout << "Empty : " << vNumbers.empty() << endl;
+	cout << "\na after calling function1 = " << a << endl;
 
 
 	return 0;
