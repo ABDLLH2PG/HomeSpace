53d730f49635b6d53b98fa941cd24c82486d13e1
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-14 15:49:17 +0300

    Lesson #36 - Dereferencing Pointer

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
 
-// Lesson #35 - What is Pointer?
+// Lesson #36 - Dereferencing Pointer
 int main()
 {
 	int a = 10;
-	
-	cout << "a value        = " << a << endl;
-	cout << "a address      = " << &a << endl;
 
-	int * p = &a;
+	cout << "a value       = " << a << endl;
+	cout << "a address     = " << &a << endl;
 
+	int * p;
+	p = &a;
 
-	cout << "Pointer Value  = " << p;
+	cout << "Pointer Value = " << p << endl;
+	cout << "Value of the address that p is pointing to is " << *p << endl;
+
+	*p = 20;
+
+	cout << a << endl;
+	cout << *p << endl;
+
+	a = 30;
+
+	cout << a << endl;
+	cout << *p << endl;
 
 	cout << endl;
 
