2ad89d72493443aaa5ab925adde1473d61d1d6bf
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-15 12:20:30 +0300

    Lesson #39 - Call by Reference: Using pointers

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 using namespace std;
 
-// Lesson #38 - Pointers vs References
+// Lesson #39 - Call by Reference: Using pointers
+void Swap(int *n1, int *n2)
+{
+	int temp;
+	temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+
+
 int main()
 {
-	int a = 10;
-	int &x = a;
-
-	cout << &a << endl;
-	cout << &x << endl;
-
-	cout << a << endl;
-	cout << x << endl;
-
-	int *p = &a;
+	int a = 1, b = 2;
 	
-	cout << p << endl;
-	cout << *p << endl;
+	cout << "Before swapping" << endl;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
 
-	int b = 20;
-	p = &b;
+	Swap(&a, &b);
 
-	cout << p << endl;
-	cout << *p << endl;
+	cout << "\nAfter swapping" << endl;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
 
 	return 0;
 }
