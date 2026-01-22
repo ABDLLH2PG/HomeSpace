841d685d9d7f793294d121c7721e96423285cd52
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-01-22 14:48:05 +0300

    Lesson #54 - Read Mode: Print file Content

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
+#include <string>
 #include <fstream>
+
 using namespace std;
 
-// Lesson #53 - Append Mode: Append Data to File
-int main()
+// Lesson #54 - Read Mode: Print file Content
+void PrintFileContenet(string FileName)
 {
 	fstream MyFile;
 
-	MyFile.open("MyFile.txt", ios::out | ios::app); //append Mode
+	MyFile.open(FileName, ios::in); //Read Mode
 
 	if (MyFile.is_open())
 	{
-		MyFile << "My name is Abdullah\n";
-		MyFile << "I have 19 Years old\n";
-		MyFile << "I love ProgrammingAdvice\n";
+		string Line;
 
+		while (getline(MyFile, Line))
+		{
+			cout << Line << endl;
+		}
 
 		MyFile.close();
 	}
+}
 
 
-	
+int main()
+{
+	PrintFileContenet("MyFile.txt");
+
+
+
 	return 0;
 }
