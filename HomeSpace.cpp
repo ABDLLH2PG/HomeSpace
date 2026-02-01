d4b3b69a5469261d7b8f23e6e691cafec9da4743
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-02-01 10:30:53 +0300

    [C07] Problem #01: 3x3 Random Matrix (My Solution)

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include "MyUtilityLib.h"
 using namespace std;
+
+// [C07] Problem #01: 3x3 Random Matrix (My Solution)
+void Fill2DArrayWithRandomNumber(int arr[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			arr[i][j] = MyUtilityLib::RandomNumber(1, 100);
+		}
+	}
+}
+
+void Print2DArrayWithRandomNumber(int arr[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+
+		cout << endl;
+	}
+}
+
 
 int main()
 {
-	cout << "I end course 6 in 2026/1/22" << endl;
+	MyUtilityLib::SeedRand();
 
+	int arr[3][3];
+	cout << "The following is a 3x3 random matrix:" << endl;
+
+	Fill2DArrayWithRandomNumber(arr);
+
+	Print2DArrayWithRandomNumber(arr);
+	
 
 	return 0;
 }
