97d6622d953e1cfc9e3aca77f824a3eb460a66e9
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-02-12 01:11:29 +0300

    Problem #18: Intersected Numbers in Matrices [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// [C07] Problem #17: Number Exists In Matrix [Optimized Code]
+// [C07] Problem #18: Intersected Numbers in Matrices [My Solution]
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
 	for (int i = 0; i < Rows; i++)
@@ -34,33 +34,54 @@ bool IsNumberInMatrix(int Matrix1[3][3], int Number, short Rows, short Cols)
 
 }
 
+void PrintIntersectedNumberInTwoMatrix(int Matrix1[3][3], int Matrix2[3][3], short Rows, short Cols)
+{
+	int arr3[10];
+	short Num = 0;
+
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			if (IsNumberInMatrix(Matrix2, Matrix1[i][j], 3, 3))
+			{
+				arr3[Num] = Matrix1[i][j];
+				Num++;
+			}
+		}
+	}
+
+	cout << "\nIntersected Numbers are:\n\n";
+	for (short k = 0; k < Num; k++)
+	{
+		cout << left << setw(3) << arr3[k] << "    ";
+	}
+}
+
 
 int main()
 {
 	int Matrix1[3][3] =
 	{
-		{77, 5, 12},
-		{22, 20, 1},
-		{1, 0, 9}
+		{77,  5, 12},
+		{22, 20,  1},
+		{1 ,  0,  9}
+	};
+
+	int Matrix2[3][3] =
+	{
+		{5 , 80, 90},
+		{22, 77,  1},
+		{10,  8, 33}
 	};
 
 	cout << "\nMatrix1:\n";
 	PrintMatrix(Matrix1, 3, 3);
 
-	int Number;
+	cout << "\nMatrix2:\n";
+	PrintMatrix(Matrix2, 3, 3);
 
-	cout << "\nPlease Enter the number to look for in matrix? ";
-	cin >> Number;
-
-
-	if (IsNumberInMatrix(Matrix1, Number, 3, 3))
-	{
-		cout << "\nYes: It is there.\n";
-	}
-	else
-	{
-		cout << "\nNo: It's Not there.\n";
-	}
+	PrintIntersectedNumberInTwoMatrix(Matrix1, Matrix2, 3, 3);
 
 	system("pause>0");
 
