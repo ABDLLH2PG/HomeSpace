0d4f580e92784f010e2cb38502289ab04b5be123
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-02-11 21:42:56 +0300

    [C07] Problem #16: Check Sparse Matrix [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// [C07] Problem #15: Count Number in Matrix [Optimized Code]
+// [C07] Problem #16: Check Sparse Matrix [My Solution]
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
 	for (int i = 0; i < Rows; i++)
@@ -18,22 +18,25 @@ void PrintMatrix(int arr[3][3], short Rows, short Cols)
 	}
 }
 
-short CountNumberInMatrix(int Matrix1[3][3], int Number, short Rows, short Cols)
+bool SparceMatric(int Matrix1[3][3], short Rows, short Cols)
 {
-	short NumberCount = 0;
+	short Counter = 0;
 
 	for (int i = 0; i < Rows; i++)
 	{
 		for (int j = 0; j < Cols; j++)
 		{
-			if (Matrix1[i][j] == Number)
+			if (Matrix1[i][j] == 0)
 			{
-				NumberCount++;
+				Counter++;
 			}
 		}
 	}
 
-	return NumberCount;
+	if (Counter > ((Rows * Cols) / 2) )
+		return true;
+	else
+		return false;
 }
 
 
@@ -41,19 +44,22 @@ int main()
 {
 	int Matrix1[3][3] =
 	{
-		{9, 1, 12},
-		{0, 9, 1},
-		{0, 9, 9}
+		{0, 0, 12},
+		{0, 0, 1},
+		{0, 0, 9}
 	};
 
 	cout << "\nMatrix1:\n";
 	PrintMatrix(Matrix1, 3, 3);
 
-	int Number;
-	cout << "\nEnter the number to count in matrix? ";
-	cin >> Number;
-
-	cout << "\nNumber " << Number << " count in matrix is " << CountNumberInMatrix(Matrix1, Number, 3, 3) << endl;
+	if (SparceMatric(Matrix1, 3, 3))
+	{
+		cout << "\nYes: It is Sparse\n";
+	}
+	else
+	{
+		cout << "\nNo: It's Not Sparce\n";
+	}
 
 
 	return 0;
