a60c7c4f7a73ebaa3822b50a5e644ce063d2bc36
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-02-28 17:56:44 +0300

    [C07] Problem #28: Invert All Letters Case [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,7 +2,7 @@
 #include <string> 
 #include "MyInputLib.h" 
 
-// [C07] Problem #27: Invert Charcter Case [Optimized Code]
+// [C07] Problem #28: Invert All Letters Case [My Solution]
 using namespace std;
 
 char InvertLetterCase(char char1)
@@ -10,14 +10,24 @@ char InvertLetterCase(char char1)
 	return isupper(char1) ? tolower(char1) : toupper(char1);
 }
 
+string InvertAllLetterCase(string S1)
+{
+	for (int i = 0; i < S1.length(); i++)
+	{
+		S1[i] = InvertLetterCase(S1[i]);
+	}
+
+	return S1;
+}
+
 
 int main()
 {
-	char Ch1 = MyInputLib::ReadChar("Please Enter a Character ?");
+	string S1 = MyInputLib::ReadString("Please Enter Your String?");
 
-	cout << "\nChar after Inverting case:\n";
-	Ch1 = InvertLetterCase(Ch1);
-	cout << Ch1 << endl;
+	cout << "\nString after Inverting All Letters Case:\n";
+	S1 = InvertAllLetterCase(S1);
+	cout << S1 << endl;
 
 	system("pause>0");
 
