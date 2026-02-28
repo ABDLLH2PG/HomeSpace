a57571cb1c1c43ef02417b999f0eb43d513fe752
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-02-28 16:08:07 +0300

    [C07] Problem #27: Invert Charcter Case [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,41 +2,25 @@
 #include <string> 
 #include "MyInputLib.h" 
 
-// [C07] Problem #26: Upper/Lower All Letters of a String [Optimized Code]
+// [C07] Problem #27: Invert Charcter Case [My Solution]
 using namespace std;
 
-string UpperAllString(string S1)
+char InvertingCharacterCase(char ch)
 {
-	for (int i = 0; i < S1.length(); i++)
-	{
-		S1[i] = toupper(S1[i]);
-	}
-
-	return S1;
-}
-
-string LowerAllString(string S1)
-{
-	for (int i = 0; i < S1.length(); i++)
-	{
-		S1[i] = tolower(S1[i]);
-	}
-
-	return S1;
+	if (ch >= 65 && ch <= 97)
+		return (ch = tolower(ch));
+	else
+		return (ch = toupper(ch));
 }
 
 
 int main()
 {
-	string S1 = MyInputLib::ReadString("Please Enter Your String ?");
+	char character = MyInputLib::ReadChar("Please Enter a Character ?");
 
-	cout << "\nString after Upper:\n";
-	S1 = UpperAllString(S1);
-	cout << S1 << endl;
-
-	cout << "\nString after Lower:\n";
-	S1 = LowerAllString(S1);
-	cout << S1 << endl;
+	cout << "\nChar after inverting case:\n";
+	character = InvertingCharacterCase(character);
+	cout << character;
 
 	system("pause>0");
 
