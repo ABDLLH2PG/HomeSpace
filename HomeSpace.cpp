0bd9b883aa38216cb90808b260785f0b888ef853
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-02-28 18:18:19 +0300

    [C07] Problem #29: Count Small/Capital Letters [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,33 +2,43 @@
 #include <string> 
 #include "MyInputLib.h" 
 
-// [C07] Problem #28: Invert All Letters Case [Optimized Code]
+// [C07] Problem #29: Count Small/Capital Letters [My Solution]
 using namespace std;
 
-char InvertLetterCase(char char1)
+int CountCapitalLetters(string S1)
 {
-	return isupper(char1) ? tolower(char1) : toupper(char1);
-}
+	int Count = 0;
 
-string InvertAllStringLetterCase(string S1)
-{
 	for (int i = 0; i < S1.length(); i++)
 	{
-		S1[i] = InvertLetterCase(S1[i]);
+		if (isupper(S1[i]))
+			Count++;
 	}
 
-	return S1;
+	return Count;
 }
 
+int CountSmallLetters(string S1)
+{
+	int Count = 0;
+
+	for (int i = 0; i < S1.length(); i++)
+	{
+		if (islower(S1[i]))
+			Count++;
+	}
+
+	return Count;
+}
 
 int main()
 {
 	string S1 = MyInputLib::ReadString("Please Enter Your String?");
 
-	cout << "\nString after Inverting All Letters Case:\n";
-	S1 = InvertAllStringLetterCase(S1);
-	cout << S1 << endl;
-
+	cout << "\nString Length = " << S1.length();
+	cout << "\nCapital Letters Count = " << CountCapitalLetters(S1);
+	cout << "\nSmall Letters Count = " << CountSmallLetters(S1);
+	
 	system("pause>0");
 
 	return 0;
