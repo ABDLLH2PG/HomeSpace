f52bd6444d6d59b1ca873006a94a197c22aecf28
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-03-01 11:51:13 +0300

    [C07] Problem #29: Count Small/Capital Letters [Optimized Code]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,34 +1,65 @@
 #include <iostream>
-#include <string> 
-#include "MyInputLib.h" 
+#include <string>
+#include "MyInputLib.h"
 
-// [C07] Problem #29: Count Small/Capital Letters [My Solution]
+// [C07] Problem #29: Count Small/Capital Letters [Optimized Code]
 using namespace std;
 
-int CountCapitalLetters(string S1)
+enum enWhatToCount { CapitalLetters = 0, SmallLetters = 1, All = 2 };
+
+short CountLetters(string S1, enWhatToCount WhatToCount = enWhatToCount::All)
 {
-	int Count = 0;
+	if (WhatToCount == enWhatToCount::All)
+	{
+		return S1.length();
+	}
+
+	short Counter = 0;
+
+	for (int i = 0; i < S1.length(); i++)
+	{
+		if (WhatToCount == enWhatToCount::CapitalLetters && isupper(S1[i]))
+		{
+			Counter++;
+		}
+
+		if (WhatToCount == enWhatToCount::SmallLetters && islower(S1[i]))
+		{
+			Counter++;
+		}
+	}
+
+	return Counter;
+}
+
+short CountCapitalLetters(string S1)
+{
+	short Counter = 0;
 
 	for (int i = 0; i < S1.length(); i++)
 	{
 		if (isupper(S1[i]))
-			Count++;
+		{
+			Counter++;
+		}
 	}
 
-	return Count;
+	return Counter;
 }
 
-int CountSmallLetters(string S1)
+short CountSmallLetters(string S1)
 {
-	int Count = 0;
+	short Counter = 0;
 
 	for (int i = 0; i < S1.length(); i++)
 	{
 		if (islower(S1[i]))
-			Count++;
+		{
+			Counter++;
+		}
 	}
 
-	return Count;
+	return Counter;
 }
 
 int main()
@@ -38,7 +69,14 @@ int main()
 	cout << "\nString Length = " << S1.length();
 	cout << "\nCapital Letters Count = " << CountCapitalLetters(S1);
 	cout << "\nSmall Letters Count = " << CountSmallLetters(S1);
-	
+
+	cout << "\n\nMethod 2:\n";
+
+	cout << "\nString Length = " << CountLetters(S1);
+	cout << "\nCapital Letters Count = " << CountLetters(S1, enWhatToCount::CapitalLetters);
+	cout << "\nSmall Letters Count = " << CountLetters(S1, enWhatToCount::SmallLetters);
+
+
 	system("pause>0");
 
 	return 0;
