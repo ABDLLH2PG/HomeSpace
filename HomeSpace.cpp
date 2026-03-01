975e7cb9fc4aa4d1f492b0d00d6079e65a65ce91
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-03-01 19:26:24 +0300

    [C07] Problem #30: Count Letters [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,28 +2,16 @@
 #include <string>
 #include "MyInputLib.h"
 
-// [C07] Problem #29: Count Small/Capital Letters [Optimized Code]
+// [C07] Problem #30: Count Letters [My Solution]
 using namespace std;
 
-enum enWhatToCount { CapitalLetters = 0, SmallLetters = 1, All = 2 };
-
-short CountLetters(string S1, enWhatToCount WhatToCount = enWhatToCount::All)
+short CountLetters(string S1, char Ch1)
 {
-	if (WhatToCount == enWhatToCount::All)
-	{
-		return S1.length();
-	}
-
 	short Counter = 0;
 
 	for (int i = 0; i < S1.length(); i++)
 	{
-		if (WhatToCount == enWhatToCount::CapitalLetters && isupper(S1[i]))
-		{
-			Counter++;
-		}
-
-		if (WhatToCount == enWhatToCount::SmallLetters && islower(S1[i]))
+		if (S1[i] == Ch1)
 		{
 			Counter++;
 		}
@@ -32,49 +20,14 @@ short CountLetters(string S1, enWhatToCount WhatToCount = enWhatToCount::All)
 	return Counter;
 }
 
-short CountCapitalLetters(string S1)
-{
-	short Counter = 0;
-
-	for (int i = 0; i < S1.length(); i++)
-	{
-		if (isupper(S1[i]))
-		{
-			Counter++;
-		}
-	}
-
-	return Counter;
-}
-
-short CountSmallLetters(string S1)
-{
-	short Counter = 0;
-
-	for (int i = 0; i < S1.length(); i++)
-	{
-		if (islower(S1[i]))
-		{
-			Counter++;
-		}
-	}
-
-	return Counter;
-}
 
 int main()
 {
-	string S1 = MyInputLib::ReadString("Please Enter Your String?");
+	string S1 = MyInputLib::ReadString("Please Enter Your String ?");
 
-	cout << "\nString Length = " << S1.length();
-	cout << "\nCapital Letters Count = " << CountCapitalLetters(S1);
-	cout << "\nSmall Letters Count = " << CountSmallLetters(S1);
+	char Ch1 = MyInputLib::ReadChar("\nPlease Enter a Character ?");
 
-	cout << "\n\nMethod 2:\n";
-
-	cout << "\nString Length = " << CountLetters(S1);
-	cout << "\nCapital Letters Count = " << CountLetters(S1, enWhatToCount::CapitalLetters);
-	cout << "\nSmall Letters Count = " << CountLetters(S1, enWhatToCount::SmallLetters);
+	cout << "\nLetter '" << Ch1 << "' Count = " << CountLetters(S1, Ch1);
 
 
 	system("pause>0");
