3233e2b9a2a05bed8bd85c3bdec11c56d69e5711
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-03-02 15:49:03 +0300

    [C07] Problem #31: Count Letter (Match Case) [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "MyInputLib.h"
 
-// [C07] Problem #30: Count Letters [Optimized Code]
+// [C07] Problem #31: Count Letter (Match Case) [My Solution]
 using namespace std;
 
 short CountLetters(string S1, char Letter)
@@ -20,6 +20,40 @@ short CountLetters(string S1, char Letter)
 	return Counter;
 }
 
+char MatchLetterCase(string S1, char Letter)
+{
+	if (Letter >= 65 && Letter <= 90)
+	{
+		return char(Letter + 32);
+	}
+
+	if (Letter >= 97 && Letter <= 122)
+	{
+		return char(Letter - 32);
+	}
+
+	else
+	{
+		return Letter;
+	}
+}
+
+short CountLettersMatchCase(string S1, char Letter)
+{
+	short Counter = 0;
+	char Ch2 = MatchLetterCase(S1, Letter);
+
+	for (int i = 0; i < S1.length(); i++)
+	{
+		if (S1[i] == Letter || S1[i] == Ch2)
+		{
+			Counter++;
+		}
+	}
+
+	return Counter;
+}
+
 
 int main()
 {
@@ -27,8 +61,11 @@ int main()
 
 	char Ch1 = MyInputLib::ReadChar("\nPlease Enter a Character ?");
 
+	char Ch2 = MatchLetterCase(S1, Ch1);
+
 	cout << "\nLetter '" << Ch1 << "' Count = " << CountLetters(S1, Ch1);
 
+	cout << "\nLetter '" << Ch1 << "' Or '" << Ch2 << "'  Count = " << CountLettersMatchCase(S1, Ch1);
 
 	system("pause>0");
 
