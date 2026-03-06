b270c05b3029071c05475611808139f0872d59e4
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-03-06 18:16:40 +0300

    [C07] Problem #32: Is Vowel? [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,43 +2,33 @@
 #include <string>
 #include "MyInputLib.h"
 
-// [C07] Problem #31: Count Letter (Match Case) [Optimized Code]
+// [C07] Problem #32: Is Vowel? [My Solution]
 using namespace std;
 
-char InvertLetterCase(char char1)
+bool IsVowel(char Letter)
 {
-	return isupper(char1) ? tolower(char1) : toupper(char1);
-}
+	char Vowel[5] = {'a','e','i','o','u'};
 
-short CountLetter(string S1, char Letter, bool MatchCase = true)
-{
-	short Counter = 0;
-
-	for (short i = 0; i < S1.length(); i++)
+	for (short i = 0; i < 5; i++)
 	{
-		if (MatchCase)
+		if (tolower(Letter) == Vowel[i])
 		{
-			if (S1[i] == Letter)
-				Counter++;
-		}
-		else
-		{
-			if (tolower(S1[i]) == tolower(Letter))
-				Counter++;
+			return true;
 		}
 	}
 
-	return Counter;
+	return false;
 }
 
 
 int main()
 {
-	string S1 = MyInputLib::ReadString("Please Enter Your String ?");
-	char Ch1 = MyInputLib::ReadChar("\nPlease Enter a Character ?");
+	char Ch1 = MyInputLib::ReadChar("Please Enter a Character ?");
 
-	cout << "\nLetter \'" << Ch1 << "\' Count = " << CountLetter(S1, Ch1);
-	cout << "\nLetter \'" << Ch1 << "\'" << " Or \'" << InvertLetterCase(Ch1) << "\'  Count = " << CountLetter(S1, Ch1, false);
+	if (IsVowel(Ch1))
+		cout << "\nYES Letter \'" << Ch1 << "\' is vowel\n";
+	else
+		cout << "\nNO Letter \'" << Ch1 << "\' is not vowel\n";
 
 
 	system("pause>0");
