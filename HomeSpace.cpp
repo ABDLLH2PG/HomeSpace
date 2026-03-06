0793706704be7e206c0f104bcfa09728ceb7ae81
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-03-06 15:46:56 +0300

    [C07] Problem #31: Count Letter (Match Case) [Optimized Code]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,52 +2,29 @@
 #include <string>
 #include "MyInputLib.h"
 
-// [C07] Problem #31: Count Letter (Match Case) [My Solution]
+// [C07] Problem #31: Count Letter (Match Case) [Optimized Code]
 using namespace std;
 
-short CountLetters(string S1, char Letter)
+char InvertLetterCase(char char1)
+{
+	return isupper(char1) ? tolower(char1) : toupper(char1);
+}
+
+short CountLetter(string S1, char Letter, bool MatchCase = true)
 {
 	short Counter = 0;
 
-	for (int i = 0; i < S1.length(); i++)
+	for (short i = 0; i < S1.length(); i++)
 	{
-		if (S1[i] == Letter)
+		if (MatchCase)
 		{
-			Counter++;
+			if (S1[i] == Letter)
+				Counter++;
 		}
-	}
-
-	return Counter;
-}
-
-char MatchLetterCase(string S1, char Letter)
-{
-	if (Letter >= 65 && Letter <= 90)
-	{
-		return char(Letter + 32);
-	}
-
-	if (Letter >= 97 && Letter <= 122)
-	{
-		return char(Letter - 32);
-	}
-
-	else
-	{
-		return Letter;
-	}
-}
-
-short CountLettersMatchCase(string S1, char Letter)
-{
-	short Counter = 0;
-	char Ch2 = MatchLetterCase(S1, Letter);
-
-	for (int i = 0; i < S1.length(); i++)
-	{
-		if (S1[i] == Letter || S1[i] == Ch2)
+		else
 		{
-			Counter++;
+			if (tolower(S1[i]) == tolower(Letter))
+				Counter++;
 		}
 	}
 
@@ -58,14 +35,11 @@ short CountLettersMatchCase(string S1, char Letter)
 int main()
 {
 	string S1 = MyInputLib::ReadString("Please Enter Your String ?");
-
 	char Ch1 = MyInputLib::ReadChar("\nPlease Enter a Character ?");
 
-	char Ch2 = MatchLetterCase(S1, Ch1);
+	cout << "\nLetter \'" << Ch1 << "\' Count = " << CountLetter(S1, Ch1);
+	cout << "\nLetter \'" << Ch1 << "\'" << " Or \'" << InvertLetterCase(Ch1) << "\'  Count = " << CountLetter(S1, Ch1, false);
 
-	cout << "\nLetter '" << Ch1 << "' Count = " << CountLetters(S1, Ch1);
-
-	cout << "\nLetter '" << Ch1 << "' Or '" << Ch2 << "'  Count = " << CountLettersMatchCase(S1, Ch1);
 
 	system("pause>0");
 
