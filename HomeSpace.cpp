6fe657052781cefabd499fd7c59660e8c4576711
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-04-01 10:28:00 +0300

    [C07] Problem #35: Print Each Word In String [Optimized Code]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,31 +2,45 @@
 #include <string>
 #include "MyInputLib.h"
 
-// [C07] Problem #35: Print Each Word In String [MySolution]
+// [C07] Problem #35: Print Each Word In String[Optimized Code]
 using namespace std;
 
-void PrintEachWord(string S1)
+void PrintEachWordInString(string S1)
 {
-	for (int i = 0; i < S1.length(); i++)
+	cout << "\nYour string words are:\n\n";
+
+	string delim = " "; // delimiter
+	short pos = 0;
+	string sWord; // define a string variable
+
+	// use find() function to get the position of the delimiters
+	while ((pos = S1.find(delim)) != string::npos)
 	{
-		if (S1[i] == ' ')
+		sWord = S1.substr(0, pos); // store the word
+
+		if (sWord != "")
 		{
-			cout << endl;
-			continue;
+			cout << sWord << endl;
 		}
 
-		cout << S1[i];
+		S1.erase(0, pos + delim.length()); 
+		/* erase() until position and move to next word. */
 	}
+
+	if (S1 != "")
+	{
+		cout << S1 << endl; // it print last word of the string.
+	}
+
 }
 
 
 int main()
 {
-	string S1 = MyInputLib::ReadString("Please Enter Your String?");
+	string S1 = MyInputLib::ReadString("Please Enter Your String ?");
 
-	cout << "\nYour String Words are:\n";
+	PrintEachWordInString(S1);
 
-	PrintEachWord(S1);
 
 	system("pause>0");
 
