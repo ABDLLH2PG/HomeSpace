56b1faac3ef33d98594585b6937e9137e5ed967d
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-04-03 23:44:32 +0300

    [C07] Problem #37: Count Each Word In String [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,46 +1,51 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "MyInputLib.h"
 
-// [C07] Problem #36: Count Each Word In String [Optimized Code]
+// [C07] Problem #37: Count Each Word In String [My Solution]
 using namespace std;
 
-short CountWords(string S1)
+void SplitWord(string S1, string delim = " ")
 {
-	string delim = " "; // delimiter
-	short Counter = 0;
+	short Tokens = 0;
 	short pos = 0;
-	string sWord; // define a string variable
+	string sWord;
+	vector <string> vWords;
 
-	// use find() function to get the position of the delimiters
 	while ((pos = S1.find(delim)) != string::npos)
 	{
-		sWord = S1.substr(0, pos); // store the word
+		sWord = S1.substr(0, pos);
 
 		if (sWord != "")
 		{
-			Counter++;
+			vWords.push_back(sWord);
+			Tokens++;
 		}
 
 		S1.erase(0, pos + delim.length());
-		/* erase() until position and move to next word. */
 	}
 
 	if (S1 != "")
 	{
-		Counter++;
+		vWords.push_back(S1);
+		Tokens++;
 	}
 
-	return Counter;
+	cout << "Tokens = " << Tokens << "\n";
+
+	for (string &vWord : vWords)
+	{
+		cout << vWord << "\n";
+	}
 }
 
 
 int main()
 {
 	string S1 = MyInputLib::ReadString("Please Enter Your String ?");
-
-	cout << "\nThe number of words in your string is: " << CountWords(S1) << endl;
 	
+	SplitWord(S1);
 
 
 	system("pause>0");
