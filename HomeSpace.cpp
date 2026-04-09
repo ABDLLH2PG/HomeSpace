a6a59b3d23d5973ef84797a6b20743294d0f4820
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-04-09 15:49:46 +0300

    [C07] Problem #40: Join String (Overloading) [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 
-// [C07] Problem #39:  Join String [Optimized Code]
+// [C07] Problem #40: Join String (Overloading) [My Solution]
 using namespace std;
 
 string JoinString(vector <string> vString, string Delim)
@@ -17,14 +17,29 @@ string JoinString(vector <string> vString, string Delim)
 	return S1.substr(0, S1.length() - Delim.length());
 }
 
+string JoinString(string arrString[], short arrLength, string Delim)
+{
+	string S1 = "";
+
+	for (short i = 0; i < arrLength; i++)
+	{
+		S1 = S1 + arrString[i] + Delim;
+	}
+
+	return S1.substr(0, S1.length() - Delim.length());
+}
 
 int main()
 {
 	vector <string> vName = { "Mohammed", "Faid", "Ali", "Maher" };
+	string arrString[4] = { "Mohammed", "Faid", "Ali", "Maher" };
+	short arrLength = 4;
 
 	cout << "\nVector after join:\n";
 	cout << JoinString(vName, " ");
 
+	cout << "\nVector after join:\n";
+	cout << JoinString(arrString, arrLength, " ");
 
 	system("pause>0");
 
