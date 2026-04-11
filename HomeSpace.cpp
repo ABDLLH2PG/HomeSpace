16ad9b52fe2b3bce1bbb862715231e8dfd7b0d7a
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-04-11 11:16:30 +0300

    [C07] Problem #43: Replace Words (Custom) [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -2,20 +2,41 @@
 #include <string>
 #include "MyInputLib.h"
 
-// [C07] Problem #42: Replace Words [Optimized Code]
+// [C07] Problem #43: Replace Words (Custom) [My Solution]
 using namespace std;
 
-string ReplaceWordInStringUsingBuiltInFunction(string S1, string StringToReplace, string ReplaceTo)
+string LowerAllString(string S1)
 {
-	short pos = S1.find(StringToReplace);
-
-	while (pos != string::npos)
+	for (int i = 0; i < S1.length(); i++)
 	{
-		S1.replace(pos, StringToReplace.length(), ReplaceTo);
-		pos = S1.find(StringToReplace); // find next
+		S1[i] = tolower(S1[i]);
 	}
 
 	return S1;
+}
+
+
+string ReplaceWordInString(string S1, string StringToReplace, string ReplaceTo, bool MatchCase = false)
+{
+	string S2 = S1;
+
+	if (!MatchCase)
+	{
+		S2 = LowerAllString(S2);
+		StringToReplace = LowerAllString(StringToReplace);
+
+	}
+
+	short pos = S2.find(StringToReplace);
+
+	while (pos != string::npos)
+	{
+		S2 = S2.erase(pos, StringToReplace.length());
+		S2 = S2.insert(pos, ReplaceTo);
+		pos = S2.find(StringToReplace);
+	}
+
+	return S2;
 }
 
 
@@ -24,8 +45,12 @@ int main()
 	string S1 = "Welcome to Jordan , Jordan is a nice country";
 	cout << "\nOrigial String\n" << S1;
 
-	cout << "\n\nString After Replace:\n";
-	cout << ReplaceWordInStringUsingBuiltInFunction(S1, "Jordan", "USA");
+	cout << "\n\nReplace with match case:\n";
+	cout << ReplaceWordInString(S1, "jordan", "USA", false);
+
+	cout << "\n\nReplace with dont match case:\n";
+	cout << ReplaceWordInString(S1, "Jordan", "USA", true);
+
 
 
 
