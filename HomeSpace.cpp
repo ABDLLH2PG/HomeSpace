2d802091537bbf5337c4c638ac23c849747c7401
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-04-11 11:43:53 +0300

    [C07] Problem #43: Replace Words (Custom) [Optimized Code]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "MyInputLib.h"
 
-// [C07] Problem #43: Replace Words (Custom) [My Solution]
+// [C07] Problem #43: Replace Words (Custom) [Optimized Code]
 using namespace std;
 
 string LowerAllString(string S1)
@@ -15,41 +16,84 @@ string LowerAllString(string S1)
 	return S1;
 }
 
-
-string ReplaceWordInString(string S1, string StringToReplace, string ReplaceTo, bool MatchCase = false)
+vector <string> SplitString(string S1, string Delim = " ")
 {
-	string S2 = S1;
+	vector <string> vString;
 
-	if (!MatchCase)
+	short pos = 0;
+	string sWord;
+
+	while ((pos = S1.find(Delim)) != string::npos)
 	{
-		S2 = LowerAllString(S2);
-		StringToReplace = LowerAllString(StringToReplace);
+		sWord = S1.substr(0, pos);
 
+		if (sWord != "")
+		{
+			vString.push_back(sWord);
+		}
+
+		S1.erase(0, pos + Delim.length());
 	}
 
-	short pos = S2.find(StringToReplace);
-
-	while (pos != string::npos)
+	if (S1 != "")
 	{
-		S2 = S2.erase(pos, StringToReplace.length());
-		S2 = S2.insert(pos, ReplaceTo);
-		pos = S2.find(StringToReplace);
+		vString.push_back(S1);
 	}
 
-	return S2;
+	return vString;
+}
+
+string JoinString(vector <string> vString, string Delim)
+{
+	string S1 = "";
+
+	for (string& s : vString)
+	{
+		S1 = S1 + s + Delim;
+	}
+
+	return S1.substr(0, S1.length() - Delim.length());
+}
+
+string ReplaceWordInStringUsingSplit(string S1, string StringToReplace, string sReplaceTo, bool MatchCase = true)
+{
+	vector <string> vString = SplitString(S1, " ");
+
+	for (string& s : vString)
+	{
+		if (MatchCase)
+		{
+			if (s == StringToReplace)
+			{
+				s = sReplaceTo;
+			}
+		}
+		else
+		{
+			if (LowerAllString(s) == LowerAllString(StringToReplace))
+			{
+				s = sReplaceTo;
+			}
+		}
+	}
+
+	return JoinString(vString, " ");
 }
 
 
 int main()
 {
 	string S1 = "Welcome to Jordan , Jordan is a nice country";
+	string StringToReplace = "jordan";
+	string sReplaceTo = "USA";
+
 	cout << "\nOrigial String\n" << S1;
 
-	cout << "\n\nReplace with match case:\n";
-	cout << ReplaceWordInString(S1, "jordan", "USA", false);
+	cout << "\n\nReplace with match case: ";
+	cout << "\n" << ReplaceWordInStringUsingSplit(S1, StringToReplace, sReplaceTo);
 
-	cout << "\n\nReplace with dont match case:\n";
-	cout << ReplaceWordInString(S1, "Jordan", "USA", true);
+	cout << "\n\nReplace with dont match case: ";
+	cout << "\n" << ReplaceWordInStringUsingSplit(S1, StringToReplace, sReplaceTo, false);
 
 
 
