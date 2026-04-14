66c63a21ebc64e054c4565edddb63db870a84c11
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-04-14 10:28:26 +0300

    [C07] Problem #46: Convert Line Data to Record [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
-#include "MyInputLib.h"
+#include <vector>
 
-// [C07] Problem #45: Convert Record to Line [Optimized Code]
+// [C07] Problem #46: Convert Line Data to Record [My Solution]
 using namespace std;
 
 struct stClient
@@ -14,52 +14,73 @@ struct stClient
 	double AccountBalance = 0.0;
 };
 
-stClient ReadNewClient()
+vector <string> SplitString(string S1, string Delim = " ")
+{
+	vector <string> vString;
+
+	short pos = 0;
+	string sWord;
+
+	while ((pos = S1.find(Delim)) != string::npos)
+	{
+		sWord = S1.substr(0, pos);
+
+		if (sWord != "")
+		{
+			vString.push_back(sWord);
+		}
+
+		S1.erase(0, pos + Delim.length());
+	}
+
+	if (S1 != "")
+	{
+		vString.push_back(S1);
+	}
+
+	return vString;
+}
+
+stClient ConvertvRecordLineToStruct(vector <string> &vRecordLine)
 {
 	stClient Client;
 
-	cout << "Enter Account Number? ";
-	getline(cin, Client.AccountNumber);
-
-	cout << "Enter PinCode? ";
-	getline(cin, Client.PinCode);
-
-	cout << "Enter Name? ";
-	getline(cin, Client.Name);
-
-	cout << "Enter Phone? ";
-	getline(cin, Client.Phone);
-
-	cout << "Enter AccountBalance? ";
-	cin >> Client.AccountBalance;
+	Client.AccountNumber = vRecordLine[0];
+	Client.PinCode = vRecordLine[1];
+	Client.Name = vRecordLine[2];
+	Client.Phone = vRecordLine[3];
+	Client.AccountBalance = stod(vRecordLine[4]);
 
 	return Client;
 }
 
-string ConvertRecordToLine(stClient Client, string Seperator = "#//#")
+void PrintClientData(stClient Client)
 {
-	string stClientRecord = "";
-
-	stClientRecord += Client.AccountNumber + Seperator;
-	stClientRecord += Client.PinCode + Seperator;
-	stClientRecord += Client.Name + Seperator;
-	stClientRecord += Client.Phone + Seperator;
-	stClientRecord += to_string(Client.AccountBalance);
-
-	return stClientRecord;
+	cout << "Account Number : " << Client.AccountNumber << "\n";
+	cout << "Pin Code       : " << Client.PinCode << "\n";
+	cout << "Name           : " << Client.Name << "\n";
+	cout << "Phone          : " << Client.Phone << "\n";
+	cout << "Account Balance: " << Client.AccountBalance;
 }
 
 
 int main()
 {
-	cout << "\nPlease Enter Client Data: \n\n";
-
 	stClient Client;
-	Client = ReadNewClient();
 
-	cout << "\n\nClient Record for Saving is: \n";
-	cout << ConvertRecordToLine(Client);
+	string S1 = "A150#//#1234#//#Mohammed Abu-Hadhoud#//#079999#//#5270.000000";
 
+	cout << "\nLine Record is:\n";
+	cout << S1;
+
+	vector <string> vRecordLine = SplitString(S1, "#//#");
+
+	Client = ConvertvRecordLineToStruct(vRecordLine);
+
+	cout << "\n\nThe following is teh extracted client record:\n\n";
+
+	PrintClientData(Client);
+	
 
 	system("pause>0");
 
