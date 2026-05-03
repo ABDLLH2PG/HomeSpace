3c14dc51bff8403730bcce41c109d59d40e5ca19
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-05-03 17:34:09 +0300

    [C08] Problem #02: Leap Year [Optimized Code]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -3,45 +3,50 @@
 #include "MyLib/MyInputLib.h"
 using namespace std;
 
-// [C08] Problem #02: Leap Year [My Solution]
+// [C08] Problem #02: Leap Year [Optimized Code]
 
-bool CheckLeapYear(int Year)
+bool IsLeapYear(short Year)
 {
+	// leap year if perfectly divisible by 400
 	if (Year % 400 == 0)
 	{
 		return true;
 	}
+
+	// not a leap year if divisible by 100
+	// but not divisible by 400
+	else if (Year % 100 == 0)
+	{
+		return false;
+	}
+
+	// leap year if not divisible by 100
+	// but divisible by 4
+	else if (Year % 4 == 0)
+	{
+		return true;
+	}
+
+	// all other years are not leap years
 	else
 	{
-		if (Year % 4 == 0 && Year % 100 != 0)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return false;
 	}
 }
 
-bool CheckLeapYear2(int Year)
-{
-	return ((Year % 400 == 0) || (Year % 4 == 0 && Year % 100 != 0));
-}
 
 int main()
 {
-	int Year = MyInputLib::ReadNumber("Enter a Year? ");
+	short Year = MyInputLib::ReadNumber("\nPlease enter a year to check? ");
 
-	if (CheckLeapYear(Year))
+	if (IsLeapYear(Year))
 	{
-		cout << Year << " is Leap Year.";
+		cout << "\nYes, Year [" << Year << "] is a leap year.\n";
 	}
 	else
 	{
-		cout << Year << " is Not Leap Year.";
+		cout << "\nNo, Year [" << Year << "] is NOT a leap year.\n";
 	}
-
 
 	system("pause>0");
 
