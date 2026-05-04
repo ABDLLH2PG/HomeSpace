75851b7be4f449410893dd2812a72d7539501de4
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-05-04 22:49:55 +0300

    [C08] Problem #07: Day Name [Optimized Code]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -3,43 +3,35 @@
 #include "MyLib/MyInputLib.h"
 using namespace std;
 
-// [C08] Problem #07: Day Name [My Solution]
+// [C08] Problem #07: Day Name [Optimized Code]
 
-void PrintDate(short Year, short Month, short Day)
+short DayOfWeekOrder(short Year, short Month, short Day)
 {
-	cout << Day << "/" << Month << "/" << Year;
+	short a, y, m;
+	a = (14 - Month) / 12;
+	y = Year - a;
+	m = Month + (12 * a) - 2;
+
+	return (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
 }
 
-short DayOrder(short Year, short Month, short Day)
+string DayShortName(short DayOfWeekOrder)
 {
-	int a = ((14 - Month) / 12);
-	int y = Year - a;
-	int m = Month + 12 * a - 2;
+	string arrDayNames[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 
-	int d = (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
-	
-	return d;
-}
-
-string DayName(short DayOrder)
-{
-	string DayName[7] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
-
-	return DayName[DayOrder];
+	return arrDayNames[DayOfWeekOrder];
 }
 
 
 int main()
 {
 	short Year = MyInputLib::ReadNumber("\nPlease enter a year? ");
-
 	short Month = MyInputLib::ReadNumber("\nPlease enter a Month? ");
-
 	short Day = MyInputLib::ReadNumber("\nPlease enter a Day? ");
 
-	cout << "\nDate      :";    PrintDate(Year, Month, Day);
-	cout << "\nDay Order : " << DayOrder(Year, Month, Day);
-	cout << "\nDay Name  : " << DayName(DayOrder(Year, Month, Day));
+	cout << "\nDate      :" << Day << "/" << Month << "/" << Year;
+	cout << "\nDay Order : " << DayOfWeekOrder(Year, Month, Day);
+	cout << "\nDay Name  : " << DayShortName(DayOfWeekOrder(Year, Month, Day));
 
 
 
