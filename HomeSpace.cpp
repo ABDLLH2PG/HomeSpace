54aa869cb0cc9295238ff0463e020bd051908ab7
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-05-24 00:02:57 +0300

    [C08] Problem #59: Period Length In Days [My Solution]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -3,7 +3,7 @@
 #include "MyLib/MyInputLib.h"
 using namespace std;
 
-// [C08] Problem #58: Is Overlap Periods [Optimized Code]
+// [C08] Problem #59: Period Length In Days [My Solution]
 
 struct stDate
 {
@@ -41,11 +41,89 @@ stPeriod ReadPeriod()
 	return Period;
 }
 
+bool IsLeapYear(short Year)
+{
+	// if year is divisible by 4 AND bot divisible by 100
+	// OR if year is divisible by 400
+	// them it is a leap year
+
+	return ((Year % 400 == 0) || (Year % 100 != 0 && Year % 4 == 0));
+}
+
+short NumberOfDaysInAMonth(short Year, short Month)
+{
+	if (Month < 1 || Month > 12)
+		return 0;
+
+	int NumberOfDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	return (Month == 2) ? IsLeapYear(Year) ? 29 : 28 : NumberOfDays[Month - 1];
+}
+
+short NumberOfDaysFromTheBeginingOfTheYear(short Day, short Month, short Year)
+{
+	short TotalDays = 0;
+
+	for (short i = 1; i <= Month - 1; i++)
+	{
+		TotalDays += NumberOfDaysInAMonth(Year, i);
+	}
+
+	return (TotalDays + Day);
+}
+
 bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
 {
 	return (Date1.Year < Date2.Year) ? true : ((Date1.Year == Date2.Year)
 		? (Date1.Month < Date2.Month ? true : (Date1.Month == Date2.Month
 			? Date1.Day < Date2.Day : false)) : false);
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+	return (Date.Day == NumberOfDaysInAMonth(Date.Year, Date.Month));
+}
+
+bool IsLastMonthInYear(short Month)
+{
+	return (Month == 12);
+}
+
+stDate IncreaseDateByOneDay(stDate Date)
+{
+	if (IsLastDayInMonth(Date))
+	{
+		if (IsLastMonthInYear(Date.Month))
+		{
+			Date.Month = 1;
+			Date.Day = 1;
+			Date.Year++;
+		}
+		else
+		{
+			Date.Day = 1;
+			Date.Month++;
+		}
+	}
+	else
+	{
+		Date.Day++;
+	}
+
+	return Date;
+}
+
+int GetDifferenceInDays(stDate Date1, stDate Date2, bool IncludeEndDay = false)
+{
+	int Days = 0;
+
+	while (IsDate1BeforeDate2(Date1, Date2))
+	{
+		Days++;
+		Date1 = IncreaseDateByOneDay(Date1);
+	}
+
+	return IncludeEndDay ? ++Days : Days;
 }
 
 bool IsDate1EqualDate2(stDate Date1, stDate Date2)
@@ -86,24 +164,20 @@ bool IsOverlapPeriods(stPeriod Period1, stPeriod Period2)
 		return true;
 }
 
+int PeriodLengthInDays(stPeriod Period, bool IncludeEndDay = false)
+{
+	return GetDifferenceInDays(Period.StartDate, Period.EndDate, IncludeEndDay);
+}
+
 
 int main()
 {
 	cout << "\nEnter Period 1:";
-	stPeriod Period1 = ReadPeriod();
+	stPeriod Period = ReadPeriod();
 
-	cout << "\nEnter Period 2:";
-	stPeriod Period2 = ReadPeriod();
+	cout << "\nPeriod Length is: " << PeriodLengthInDays(Period);
+	cout << "\nPeriod Length (Including End Date) is: " << PeriodLengthInDays(Period, true);
 
-
-	if (IsOverlapPeriods(Period1, Period2))
-	{
-		cout << "\nYes, Periods Overlap\n";
-	}
-	else
-	{
-		cout << "\nNo, Periods do not Overlap\n";
-	}
 
 	system("pause>0");
 
