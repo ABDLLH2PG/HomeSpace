961b67caa75369b9fb12fc2b0b0cbc028a4caeb3
ABDLLH2PG <ABDLLH2PG@gmail.com>
2026-05-25 17:23:55 +0300

    [C08] Problem #61: Count Overlap Days [Optimized Code]

--- HomeSpace.cpp
+++ HomeSpace.cpp
@@ -3,7 +3,7 @@
 #include "MyLib/MyInputLib.h"
 using namespace std;
 
-// [C08] Problem #61: Count Overlap Days [My Solution]
+// [C08] Problem #61: Count Overlap Days [Optimized Code]
 
 struct stDate
 {
@@ -178,19 +178,36 @@ bool IsDateInPeriod(stDate Date, stPeriod Period)
 
 int CountOverlapDays(stPeriod Period1, stPeriod Period2)
 {
-	int Days = 0;
+	int Period1Length = PeriodLengthInDays(Period1, true);
+	int Period2Length = PeriodLengthInDays(Period2, true);
+	int OverlapDays = 0;
 
-	if (CompareDates(Period2.StartDate, Period1.EndDate) == enDateCompare::Before)
+	if (!IsOverlapPeriods(Period1, Period2))
+		return 0;
+
+	if (Period1Length < Period2Length)
 	{
-		Days = GetDifferenceInDays(Period2.StartDate, Period1.EndDate);
+		while (IsDate1BeforeDate2(Period1.StartDate, Period1.EndDate))
+		{
+			if (IsDateInPeriod(Period1.StartDate, Period2))
+				OverlapDays++;
+
+			Period1.StartDate = IncreaseDateByOneDay(Period1.StartDate);
+
+		}
 	}
-	if (CompareDates(Period2.EndDate, Period1.StartDate) == enDateCompare::Before)
+	else
 	{
-		Days = GetDifferenceInDays(Period2.EndDate, Period1.StartDate);
+		while (IsDate1BeforeDate2(Period2.StartDate, Period2.EndDate))
+		{
+			if (IsDateInPeriod(Period2.StartDate, Period1))
+				OverlapDays++;
+
+			Period2.StartDate = IncreaseDateByOneDay(Period2.StartDate);
+		}
 	}
-
-
-	return Days;
+	
+	return OverlapDays;
 }
 
 
@@ -203,7 +220,6 @@ int main()
 	stPeriod Period2 = ReadPeriod();
 	
 	cout << "\nOverlap Days Count Is: " << CountOverlapDays(Period1, Period2);
-
 
 
 	system("pause>0");
